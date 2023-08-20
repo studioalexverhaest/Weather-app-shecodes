ef867d0c429be9aa23d7c5aa6628fe97eacdd6e6
studioalexverhaest <81089496+studioalexverhaest@users.noreply.github.com>
2023-08-20 20:58:24 +0200

    this version works
    but the image icon is still broken
    conversion to fahrenheit works, but conversion to celcius doesn't
    week temp etc is still mockup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 // date and time display local
-
 let now = new Date();
 console.log(now.getDay());
-
 const body = document.querySelector("body");
 body.onload = setToday;
 body.onload = setTime;
-
 function setToday() {
   let day = document.querySelector("#day");
   let today = now.getDay();
@@ -22,42 +19,56 @@ function setToday() {
   today = weekdays[now.getDay()];
   day.innerHTML = `${today}`;
 }
-
 setToday();
-
 function setTime() {
   let currentTime = document.querySelector("#time");
-
   let theHour = now.getHours();
   let theMinutes = now.getMinutes();
-
   currentTime.innerHTML = `${theHour} : ${theMinutes}`;
 }
-
 setTime();
-
 // search city reflected in UI + livesearch
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector(`#searchBar`);
   let cityName = document.querySelector(`#yourCity`);
   // let cityName = cityName.toUpperCase();
-
   cityName.innerHTML = `${searchInput.value}`;
-
   //  cityName = cityName.toUpperCase();
 }
-
 let form = document.querySelector("#searchCityBar");
 form.addEventListener("submit", search);
 
-////// IMPLEMENT SEARCH ENGINE
+// switch temp between Farenheit and Celcius
+// function switchTemp(event) {
+//   let degreeNumber = document.querySelector(`#degrees`);
+//   let farenheitSymbol = document.querySelector(`#farenheitSymbol`);
+//   degreeNumber.innerHTML = `56`;
+//   farenheitSymbol.innerHTML = `C`;
+//   // let celciusSymbol = document.querySelector(`#celciusSymbol`);
+//   // celciusSymbok.innerHTML = `F`;
+// }
+// function switchTemp(event) {
+//   let degreeNumber = document.querySelector(`#degrees`);
+//   let farenheitSymbol = document.querySelector(`#farenheitSymbol`);
+//   degreeNumber.innerHTML = `56`;
+//   farenheitSymbol.innerHTML = `C`;
+// let celciusSymbol = document.querySelector(`#celciusSymbol`);
+// celciusSymbok.innerHTML = `F`;
+// }
 
+// let weirdTemp = document.querySelector(`#farenheitLink`);
+// weirdTemp.addEventListener("click", switchTemp);
+// function showWeather(response) {
+//   console.log(response);
+// }
+////// IMPLEMENT SEARCH ENGINE
 function handleSearch(response) {
   console.log(response.data);
   let returnTemp = document.querySelector(`#degrees`);
 
   let temperature = Math.round(response.data.main.temp);
+  returnTemp.innerHTML = `${temperature}°C`;
   returnTemp.innerHTML = `${temperature}`;
   let returnMood = document.querySelector(`#mood`);
   returnMood.innerHTML = `${response.data.weather[0].description}`;
@@ -73,33 +84,49 @@ function handleSearch(response) {
   );
 }
 
-// // local-button
+//search on load
+// function search(city) {
+//   let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//   axios.get(apiUrl).then(handleSearch);
+// }
 
+// search-bar
+function liveSearch(position) {
+  let searchInput = document.querySelector(`#searchBar`);
+
+  let cityName = document.querySelector(`#yourCity`);
+  cityName = `${searchInput.value}`;
+
+  let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(handleSearch);
+  console.log(cityName);
+}
+form.addEventListener("submit", liveSearch);
+
+// // local-button
 let localButton = document.querySelector(`#locality`);
 localButton.addEventListener("click", handleButton);
-
 function handleButton(event) {
   navigator.geolocation.getCurrentPosition(retrievePosition);
   console.log("click");
 }
-
 function retrievePosition(position) {
   console.log(position);
   let apiKey = "64469ac67e6dc941feb5b50915a18dc7";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-
   axios.get(url).then(handleLocalSearch);
 }
-
 function handleLocalSearch(response) {
   console.log(response.data.sys.name);
-
   let returnCity = document.querySelector(`#yourCity`);
   returnCity.innerHTML = `here`;
   let returnTemp = document.querySelector(`#degrees`);
   let temperature = Math.round(response.data.main.temp);
+  returnTemp.innerHTML = `${temperature}°C`;
   returnTemp.innerHTML = `${temperature}`;
   let returnMood = document.querySelector(`#mood`);
   returnMood.innerHTML = `${response.data.weather[0].description}`;
@@ -135,3 +162,5 @@ let celciusIcon = document.querySelector(`#celcius-icon`);
 fahrenheitIcon.addEventListener("click", displayCelciusTemperature);
 
 let celciusTemperature = null;
+
+// search("New York");
