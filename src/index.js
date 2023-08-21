ac4e1d6ddf3a5d2b3d486ae9711dcd83a9cfcf7e
studioalexverhaest <81089496+studioalexverhaest@users.noreply.github.com>
2023-08-21 10:29:30 +0200

    need better feedback

--- src/index.js
+++ src/index.js
@@ -40,10 +40,12 @@ let form = document.querySelector("#searchCityBar");
 form.addEventListener("submit", search);
 
 ////// IMPLEMENT SEARCH ENGINE
-function handleSearch(response) {
+function showWeather(response) {
   console.log(response.data);
+  let cityName = document.querySelector(`#yourCity`);
+  cityName.innerHTML = `${response.data.name}`;
+  celciusTemperature = response.data.main.temp;
   let returnTemp = document.querySelector(`#degrees`);
-  //  let celciusTemperature = response.data.main.temp;
   let temperature = Math.round(response.data.main.temp);
   returnTemp.innerHTML = `${temperature}`;
   let returnMood = document.querySelector(`#mood`);
@@ -60,37 +62,17 @@ function handleSearch(response) {
 }
 
 // SEARCH-BAR
-function liveSearch(response) {
+function liveSearch(event) {
+  event.preventDefault();
   let searchInput = document.querySelector(`#searchBar`);
-  let cityName = document.querySelector(`#yourCity`);
-  cityName = `${searchInput.value}`;
-
-  let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(handleSearch);
-  console.log(cityName);
+  searchCity(searchInput.value);
 }
 form.addEventListener("submit", liveSearch);
 
-// LOCAL-BUTTON
-function handleLocalSearch(response) {
-  console.log(response.data.sys.name);
-  let returnCity = document.querySelector(`#yourCity`);
-  returnCity.innerHTML = `here`;
-  let returnTemp = document.querySelector(`#degrees`);
-  let temperature = Math.round(response.data.main.temp);
-  returnTemp.innerHTML = `${temperature}`;
-  let returnMood = document.querySelector(`#mood`);
-  returnMood.innerHTML = `${response.data.weather[0].description}`;
-  let returnWind = document.querySelector(`#wind`);
-  returnWind.innerHTML = `wind : ${response.data.wind.speed} km/h`;
-  let returnPrcp = document.querySelector(`#prcp`);
-  returnPrcp.innerHTML = `Prcp : ${response.data.main.humidity} %`;
-  let bigIcon = document.querySelector(`#bigIcon`);
-  bigIcon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+function searchCity(city) {
+  let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
 }
 
 function retrievePosition(position) {
@@ -98,65 +80,37 @@ function retrievePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(handleLocalSearch);
+  axios.get(url).then(showWeather);
 }
 
 function handleButton(event) {
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrievePosition);
-  console.log("click");
 }
 
 let localButton = document.querySelector(`#locality`);
 localButton.addEventListener("click", handleButton);
 
-//CONVERSION TO FAHRENHEIT
-// function displayFahrenheitTemperature(event) {
-//   event.preventDefault();
-//   let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-//   let temperature = document.querySelector(`#degrees`);
-//   temperature.innerHTML = `${Math.round(fahrenheitTemperature)}`;
-// }
+// CONVERSION TO FAHRENHEIT
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  let temperature = document.querySelector(`#degrees`);
+  temperature.innerHTML = `${Math.round(fahrenheitTemperature)}`;
+}
 
-// let fahrenheitIcon = document.querySelector(`#fahrenheit-icon`);
-// fahrenheitIcon.addEventListener("click", displayFahrenheitTemperature);
+let fahrenheitIcon = document.querySelector(`#fahrenheit-icon`);
+fahrenheitIcon.addEventListener("click", displayFahrenheitTemperature);
 
 // back to celcius does not work
-// function displayCelciusTemperature(event) {
-//   event.preventDefault();
-//   let celciusTemperature = celciusTemperature;
-//   let temperature = document.querySelector(`#degrees`);
-//   temperature.innerHTML = `${Math.round(celciusTemperature)}`;
-// }
+function displayCelciusTemperature(event) {
+  event.preventDefault();
+  let temperature = document.querySelector(`#degrees`);
+  temperature.innerHTML = `${Math.round(celciusTemperature)}`;
+}
 
-// let celciusIcon = document.querySelector(`#celcius-icon`);
-// fahrenheitIcon.addEventListener("click", displayCelciusTemperature);
+let celciusIcon = document.querySelector(`#celcius-icon`);
+celciusIcon.addEventListener("click", displayCelciusTemperature);
 
-//SEARCH ON LOAD
-// function searchOnLoad(response) {
-//   alert("doesthiswork?");
-//   let returnCity = document.querySelector(`#yourCity`);
-//   returnCity.innerHTML = `Seoul`;
-//   let returnTemp = document.querySelector(`#degrees`);
-//   let temperature = Math.round(response.data.main.temp);
-//   returnTemp.innerHTML = `${temperature}`;
-//   let returnMood = document.querySelector(`#mood`);
-//   returnMood.innerHTML = `${response.data.weather[0].description}`;
-//   let returnWind = document.querySelector(`#wind`);
-//   returnWind.innerHTML = `wind : ${response.data.wind.speed} km/h`;
-//   let returnPrcp = document.querySelector(`#prcp`);
-//   returnPrcp.innerHTML = `Prcp : ${response.data.main.humidity} %`;
-//   let bigIcon = document.querySelector(`#bigIcon`);
-//   bigIcon.setAttribute(
-//     "src",
-//     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-//   );
-// }
-
-// search("Seoul");
-// function search(loadCity) {
-//   let apiKey = "082d3d02ffdb12f2fd9b259e2ced1d0d";
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${loadCity}&appid=${apiKey}&units=metric`;
-//   axios.get(apiUrl).then(searchOnLoad);
-// }
-
-// let celciusTemperature = null;
+let celciusTemperature = null;
+searchCity("Schellebelle");
